2efa22852189f55b50b9281617d1f71bd28ad4b9
dvarrui <dvarrui@protonmail.com>
2020-04-28 11:25:41 +0100

    N

--- demos/prueba/paso3/player3.gd
+++ demos/prueba/paso3/player3.gd
@@ -1,17 +1,11 @@
 extends KinematicBody2D
 
 export var speed = 12000
-var bullet_resource
 
 func _ready():
 	$sprite.animation = "default"
-	bullet_resource = load("res://paso2/bullet.tscn")
 
 func _physics_process(delta):
-	update_motion(delta)
-	update_shot(delta)
-
-func update_motion(delta):
 	var motion = Vector2()
 	
 	if Input.is_action_pressed("player_up"):
@@ -31,11 +25,3 @@ func update_motion(delta):
 
 	motion = motion.normalized() * speed * delta
 	move_and_slide(motion)
-
-func update_shot(delta):
-	if Input.is_action_just_pressed("player_shot"):
-		var bullet = bullet_resource.instance()
-		bullet.position.x = self.position.x + 32
-		self.add_child(bullet)
-		bullet.apply_impulse(Vector2(), Vector2(2500,-400))
-
