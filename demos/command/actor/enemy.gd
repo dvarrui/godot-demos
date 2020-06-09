5e6bbc5b9adba97e8f32298e40c8a0096c4ed809
dvarrui <dvarrui@protonmail.com>
2020-06-09 14:06:04 +0100

    U

--- demos/command/actor/enemy.gd
+++ demos/command/actor/enemy.gd
@@ -5,15 +5,19 @@ var rot_speed = 5
 var bullet_res = load("res://actor/bullet.tscn") 
 var max_wait_time = 1
 var acc_wait_time = 0
+var player = null 
 
 func _physics_process(delta):
 	if state=="idle":
 		return
 	if state=="shot":
-		var dir = Vector2(cos(rotation), sin(rotation))
-		shot(dir)
-		acc_wait_time = 0
-		state="wait"
+		# https://docs.godotengine.org/es/stable/tutorials/math/vector_math.html
+		# Only shot when enemy can see player
+		var Enemy2Player = self.position.direction_to(player.position)
+		if Enemy2Player.dot(self.transform.x) > 0:
+			shot(self.transform.x)
+			acc_wait_time = 0
+			state="wait"
 	if state=="wait":
 		acc_wait_time += delta
 		if acc_wait_time >= max_wait_time:
@@ -26,6 +30,7 @@ func hurt():
 func _on_env_body_entered(body):
 	if body.name=="player":
 		state="shot"
+		player = body
 
 func _on_env_body_exited(body):
 	if body.name=="player":
