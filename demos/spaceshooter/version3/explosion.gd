efef0075f24b033d1d80a93aa2ef473d9565b86d
dvarrui <dvarrui@protonmail.com>
2020-06-27 19:57:03 +0100

    U

--- demos/spaceshooter/version3/explosion.gd
+++ demos/spaceshooter/version3/explosion.gd
@@ -5,18 +5,11 @@ var acc_life = 0
 
 func _ready():
 	max_life = max($fire.lifetime, $fire/pieces.lifetime) + 0.5
-	$fire.visible = true
-	$fire.one_shot = true
-	$fire.emitting = true
-	$fire/pieces.visible = true
-	$fire/pieces.one_shot = true
-	$fire/pieces.emitting = true
-	$fire/smoke.visible = true
-	$fire/smoke.one_shot = true 
-	$fire/smoke.emitting = true
-	$fire/smoke2.visible = true
-	$fire/smoke2.one_shot = true 
-	$fire/smoke2.emitting = true
+	var items = [ $fire, $fire/pieces, $fire/smoke, $fire/smoke2 ]
+	for item in items:
+		item.visible = true
+		item.one_shot = true
+		item.emitting = true
 
 func _process(delta):
 	acc_life += delta
