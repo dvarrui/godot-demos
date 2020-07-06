8857ef2f60058169a60ce6de4e73505f07477558
dvarrui <dvarrui@protonmail.com>
2020-07-06 13:49:53 +0100

    U improve jump

--- demos/platform/actor/player.gd
+++ demos/platform/actor/player.gd
@@ -1,18 +1,39 @@
 extends KinematicBody2D
 
 export var speed = 100
-export var gravity = 170
+export var default_gravity = 170
+export var jump_speed = -200
+var state = "walking"
+var current_gravity = 0
+var previous_pos = Vector2.ZERO
 
 func _ready():
-	pass # Replace with function body.
+	previous_pos = position
+	current_gravity = default_gravity
 
 # warning-ignore:unused_argument
 func _physics_process(delta):
+	if abs(previous_pos.y - position.y) < 0.1:
+		if state=="falling":
+			state = "walking"
+		elif state=="jumping":
+			state="falling"
+			current_gravity = default_gravity
 	var motion = Vector2.ZERO
 	if Input.is_action_pressed("player_left"):
 		motion.x = -speed
 	if Input.is_action_pressed("player_right"):
 		motion.x = speed
-	motion.y = gravity
+	if Input.is_action_just_pressed("player_jump") and state == "walking":
+		current_gravity = jump_speed
+		state = "jumping"
+		$jump_timer.start()
+	motion.y = current_gravity
+	previous_pos = position
 # warning-ignore:return_value_discarded
 	move_and_slide(motion)
+
+
+func _on_jump_timer_timeout():
+	current_gravity = default_gravity
+	state = "falling"
