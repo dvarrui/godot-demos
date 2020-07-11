6a900f8caf0c7dcfed6eb0b4e1703918dda569c0
dvarrui <dvarrui@protonmail.com>
2020-07-11 20:08:29 +0100

    N stairs working. Use up and down key to move player

--- demos/platform/actor/player.gd
+++ demos/platform/actor/player.gd
@@ -1,19 +1,17 @@
 extends KinematicBody2D
 
-var input_direction = 0 # Input X direction
-var direction = 0       # Current X direction
+# Input
+var input_direction = Vector2.ZERO # Input XY direction
+var direction = Vector2.ZERO       # Current XY direction
+var speed = Vector2.ZERO # Character speed
 
-var speed_x = 0 # Horizontal speed about character
-var speed_y = 0 # Vertical spped about character
-var motion = Vector2()
-
-const MAX_SPEED_X = 200 # 200
+const MAX_SPEED = Vector2(200,400) # 200, 400
 const ACCELERATION = 600
 const DECELERATION = 1300 # 2000
-
-const MAX_SPEED_Y = 400
 const JUMP_FORCE = 210 # 210
 const GRAVITY = 330 # 330
+
+var on_stairs = false
 
 func _ready():
 	#set_process(true)
@@ -22,48 +20,66 @@ func _ready():
 
 func _input(event):
 	if event.is_action_pressed("player_jump") and is_on_floor():
-		speed_y = -JUMP_FORCE
+		speed.y = -JUMP_FORCE
 
 func _physics_process(delta):
-#func _process(delta):
-	# INPUT
-	if is_on_floor():
-		if input_direction:
-			direction = input_direction
+	# INPUT horizontal direction
+	if is_on_floor() or on_stairs:
+		if input_direction.x:
+			direction.x = input_direction.x
 		if Input.is_action_pressed("player_left"):
-			input_direction = -1
+			input_direction.x = -1
 		elif Input.is_action_pressed("player_right"):
-			input_direction = 1
+			input_direction.x = 1
 		else:
-			input_direction = 0
-	
-	# MOVEMENT
-	if is_on_floor():
-		if input_direction == - direction:
-			speed_x /= 3
-		elif input_direction:
-			speed_x += ACCELERATION * delta
+			input_direction.x = 0
+	if on_stairs:
+		direction.y = 0
+		if Input.is_action_pressed("player_up"):
+			direction.y = -1
+		if Input.is_action_pressed("player_down"):
+			direction.y = 1
+		
+	# MOVEMENT restrictions
+	if is_on_floor() or on_stairs:
+		# Horizontal movement
+		if input_direction.x == - direction.x:
+			speed.x /= 3
+		elif input_direction.x:
+			speed.x += ACCELERATION * delta
 		else:
-			speed_x -= DECELERATION * delta
-		speed_x = clamp(speed_x, 0, MAX_SPEED_X)
+			speed.x -= DECELERATION * delta
+		speed.x = clamp(speed.x, 0, MAX_SPEED.x)
 	elif is_on_wall():
 		# Limit X speed when collide with wall and is jumping
-		speed_x = clamp(speed_x, 0, MAX_SPEED_X/5)
+		speed.x = clamp(speed.x, 0, MAX_SPEED.x/5)
 	elif is_on_ceiling():
 		# Limit Y speed when collide with ceiling collide
-		speed_y = 0
+		speed.y = 0
 	else:
 		# Limit max X speed when jumping
-		speed_x = clamp(speed_x, 0, MAX_SPEED_X/3 +15)
-	
-	speed_y += GRAVITY * delta
-	speed_y = clamp(speed_y, -MAX_SPEED_Y, MAX_SPEED_Y)
+		speed.x = clamp(speed.x, 0, MAX_SPEED.x/3 +15)
 
-	motion.x = speed_x * direction
-	motion.y = speed_y
+	if on_stairs:
+		speed.y = MAX_SPEED.y/3 * direction.y
+	else:
+		speed.y += GRAVITY * delta
+		speed.y = clamp(speed.y, -MAX_SPEED.y, MAX_SPEED.y)
+
+	var motion = Vector2()
+	motion.x = speed.x * direction.x
+	motion.y = speed.y
 # warning-ignore:return_value_discarded
 	move_and_slide(motion, Vector2(0, -1))
 #	move_and_slide_with_snap(motion, Vector2.DOWN, Vector2.UP)
 
 func _on_visibility_screen_exited():
 	get_tree().quit()
+
+func _on_detect_area_entered(area):
+	if area.is_in_group("stairs"):
+		on_stairs = true
+
+func _on_detect_area_exited(area):
+	if area.is_in_group("stairs"):
+		on_stairs = false
