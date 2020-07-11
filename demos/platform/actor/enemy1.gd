3eb1ffa6077a547ebd8a863246591ffc7efe266d
dvarrui <dvarrui@protonmail.com>
2020-07-11 12:43:44 +0100

    U enemy1 animation

--- demos/platform/actor/enemy1.gd
+++ demos/platform/actor/enemy1.gd
@@ -2,8 +2,17 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$anim.play("sleep")
 
 func _on_enemy1_body_entered(body):
 	if body.is_in_group("player"):
 		get_tree().quit()
+
+func _on_detect_body_entered(body):
+	if body.is_in_group("player"):
+		$anim.play("idle")
+	pass # Replace with function body.
+
+func _on_detect_body_exited(body):
+	if body.is_in_group("player"):
+		$anim.play("sleep")
