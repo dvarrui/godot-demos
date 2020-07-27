81998cfd0834fb8ee59912d274f87195dac48fe1
dvarrui <dvarrui@protonmail.com>
2020-07-27 15:37:09 +0100

    U satelite shot left and right

--- demos/spaceshooter/actor/satelite.gd
+++ demos/spaceshooter/actor/satelite.gd
@@ -2,19 +2,23 @@ extends Area2D
 
 export var speed_y = 10
 export var life = 4
+export var can_shot_timeout = 0.75
 var bullet_res = null
 var explosion_res = null
 var player = null
+var can_shot = true
 
 func _ready():
 	bullet_res = preload("res://actor/bullet_down.tscn")
 	explosion_res = preload("res://world/explosion.tscn")
-	$timer.stop()
+	$timer/flash.stop()
 	player = get_tree().root.get_node("level/player")
 
 func _process(delta):
 	update_movement(delta)
-	if abs(position.y-player.position.y)< 5:
+	if player==null:
+		return
+	if abs(position.y-player.position.y) < 50:
 		if position.x < player.position.x:
 			shot("right")
 		else:
@@ -35,20 +39,34 @@ func hit():
 	life -= 1
 	if life > 0:
 		$sprite.self_modulate = Color(0.5, 0.5, 0.5, 1)
-		$timer.start(0.15)
+		$timer/flash.start(0.15)
 		shot("down")
 	else:
 		explode()
 
-func _on_timer_timeout():
+func _on_flash_timeout():
 	$sprite.self_modulate = Color(1, 1, 1, 1)
-	$timer.stop()
+	$timer/flash.stop()
 
 func shot(direction):
-	print("[INFO] satelite shot "+direction)
+	if not can_shot:
+		return
 	var bullet = bullet_res.instance()
-	bullet.position = self.position + Vector2(0, +40)
+	if direction in ["left","right"]:
+		can_shot = false
+		$timer/can_shot.start(can_shot_timeout)
+		bullet.position = self.position + Vector2(0, 0)
+		bullet.speed_y = Global.camera_speed
+		bullet.speed_x = -300
+		if direction == "right":
+			bullet.speed_x = 300
+		bullet.rotate(1.57)
+	else:
+		bullet.position = self.position + Vector2(0, +40)
 	get_parent().add_child(bullet)
+
+func _on_can_shot_timeout():
+	can_shot = true
 
 func explode():
 	var explosion = explosion_res.instance()
