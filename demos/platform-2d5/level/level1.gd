072d258a22a5d311fcce84bcbd05549581c64964
dvarrui <dvarrui@protonmail.com>
2020-08-06 17:25:12 +0100

    N image

--- demos/platform-2d5/level/level1.gd
+++ demos/platform-2d5/level/level1.gd
@@ -6,10 +6,3 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed("game_quit"):
 		get_tree().quit()
-	
-	var x = $player.position.x 
-	var y = $player.position.y
-	$info/position.text = "("+str(int(x))+","+str(int(y))+")"
-	$info/height.text = "h"+str($player.height)
-	$info/tile.text = str($player.get_cell_id($player.get_cell_coord()))
-
