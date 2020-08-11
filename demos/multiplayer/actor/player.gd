1481496304eb0f66685f11e3b9b671325798c750
dvarrui <dvarrui@protonmail.com>
2020-08-11 16:43:38 +0100

    U

---
+++ demos/multiplayer/actor/player.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+var speed = 300
+var motion = Vector2.ZERO
+
+func _ready():
+	$label.text = self.name 
+
+func _process(delta):
+
+	# Is the master of the paddle.
+	if is_network_master():
+		var dir = Vector2.ZERO
+		if Input.is_action_pressed("player_left"):
+			dir += Vector2(-1, 0)
+		if Input.is_action_pressed("player_right"):
+			dir += Vector2(1, 0)
+		if Input.is_action_pressed("player_up"):
+			dir += Vector2(0, -1)
+		if Input.is_action_pressed("player_down"):
+			dir += Vector2(0, 1)
+	
+		motion = dir * speed * delta	
+		# Using unreliable to make sure position is updated as fast
+		# as possible, even if one of the calls is dropped.
+		rpc_unreliable("set_pos_and_motion", position, motion)
+	else:
+		pass
+	
+	translate(motion)
+
+# Synchronize position and speed to the other peers.
+puppet func set_pos_and_motion(p_pos, p_motion):
+	position = p_pos
+	motion = p_motion
--- demos/multiplayer/level/level1.gd
+++ demos/multiplayer/level/level1.gd
@@ -2,6 +2,20 @@ extends Node2D
 
 signal game_finished()
 
+onready var player1 = $player1
+onready var player2 = $player2
+
+func _ready():
+	# By default, all nodes in server inherit from master,
+	# while all nodes in clients inherit from puppet.
+	# set_network_master is tree-recursive by default.
+	if get_tree().is_network_server():
+		# For the server, give control of player 2 to the other peer.
+		player2.set_network_master(get_tree().get_network_connected_peers()[0])
+	else:
+		# For the client, give control of player 2 to itself.
+		player2.set_network_master(get_tree().get_network_unique_id())
+
 func _process(delta):
-	if Input.action_press("game_exit"):
+	if Input.is_action_pressed("game_exit"):
 		emit_signal("game_finish")
