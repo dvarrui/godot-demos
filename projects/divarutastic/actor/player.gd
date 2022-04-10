1a49e2633d6bdf9cd08925267d900e543640aaed
David Vargas <dvarrui@protonmail.com>
2022-04-10 04:16:08 +0100

    [U] .

--- projects/divarutastic/actor/player.gd
+++ projects/divarutastic/actor/player.gd
@@ -5,10 +5,10 @@ var input_direction = Vector2.ZERO # Input XY direction
 var direction = Vector2.ZERO       # Current XY direction
 var speed = Vector2.ZERO # Character speed
 
-const MAX_SPEED = Vector2(200,400) # 200, 400
+const MAX_SPEED = Vector2(300,300) # 200, 400
 const ACCELERATION = 600
 const DECELERATION = 1300 # 2000
-const JUMP_FORCE = 210 # 210
+const JUMP_FORCE = 200 # 210
 const GRAVITY = 250 # 330
 
 var on_stairs = false
@@ -24,18 +24,21 @@ func _input(event):
 	if event.is_action_pressed("player_jump") and is_on_floor():
 		speed.y = -JUMP_FORCE
 
-
 func _physics_process(delta):
+	###
 	# INPUT horizontal direction
-	if is_on_floor() or on_stairs:
-		if input_direction.x:
-			direction.x = input_direction.x
-		if Input.is_action_pressed("player_left"):
-			input_direction.x = -1
-		elif Input.is_action_pressed("player_right"):
-			input_direction.x = 1
-		else:
-			input_direction.x = 0
+	#if is_on_floor() or on_stairs:
+	if input_direction.x:
+		direction.x = input_direction.x
+	if Input.is_action_pressed("player_left"):
+		input_direction.x = -1
+	elif Input.is_action_pressed("player_right"):
+		input_direction.x = 1
+	else:
+		input_direction.x = 0
+
+	###
+	# Vertical movement	
 	if on_stairs:
 		direction.y = 0
 		if Input.is_action_pressed("player_up"):
@@ -44,19 +47,19 @@ func _physics_process(delta):
 			direction.y = 1
 		
 	# MOVEMENT restrictions
-	if is_on_floor() or on_stairs:
-		# Horizontal movement
-		if input_direction.x == - direction.x:
-			speed.x /= 3
-		elif input_direction.x:
-			speed.x += ACCELERATION * delta
-		else:
-			speed.x -= DECELERATION * delta
-		speed.x = clamp(speed.x, 0, MAX_SPEED.x)
-	elif is_on_wall():
-		# Limit X speed when collide with wall and is jumping
-		speed.x = clamp(speed.x, 0, MAX_SPEED.x/5)
-	elif is_on_ceiling():
+	# if is_on_floor() or on_stairs:
+	# Horizontal movement
+	if input_direction.x == - direction.x:
+		speed.x /= 3
+	elif input_direction.x:
+		speed.x += ACCELERATION * delta
+	else:
+		speed.x -= DECELERATION * delta
+	speed.x = clamp(speed.x, 0, MAX_SPEED.x)
+	#elif is_on_wall():
+	# Limit X speed when collide with wall and is jumping
+	#	speed.x = clamp(speed.x, 0, MAX_SPEED.x/5)
+	if is_on_ceiling():
 		# Limit Y speed when collide with ceiling collide
 		speed.y = 0
 	else:
@@ -79,7 +82,10 @@ func _physics_process(delta):
 func update_anim(motion):
 	# ANIM
 	if on_stairs:
-		$anim.play("stairs")
+		if motion.y==0:
+			$anim.play("stairs-dle")
+		else:
+			$anim.play("stairs")
 	elif is_on_floor():
 		if motion.x==0:
 			$anim.play("idle")
