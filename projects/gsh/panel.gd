d6be007d1858bf1b1abc9b44225c656c952ac61b
David Vargas <dvarrui@protonmail.com>
2022-05-20 22:06:26 +0100

    [N] .

---
+++ projects/gsh/panel.gd
@@ -0,0 +1,19 @@
+extends Panel
+
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_button_pressed():
+	# Reading command
+	var command = get_node("textedit").text
+	get_node("textedit").text = ""
+	
+	# Executing command
+	print("[INFO] Executing: ", command)
+	var output = []
+	var exitcode = OS.execute(command, [], true, output)
+
+	# Displaying command output	
+	get_node("label").text += output[0]
+	print(output)
