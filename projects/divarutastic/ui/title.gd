ab062f97eb58020ca835478d6f04c9dd11177df8
David Vargas <dvarrui@protonmail.com>
2022-07-13 11:26:30 +0100

    [U] .

---
+++ projects/divarutastic/ui/title.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+func _on_start_pressed():
+	start_new_game()
+
+func _on_quit_pressed():
+	close_game()
+
+func _process(delta):
+	if Input.is_action_just_pressed("game_new"):
+		start_new_game()
+	if Input.is_action_just_pressed("game_quit"):
+		close_game()
+
+func start_new_game():
+	get_tree().change_scene("res://levels/level01.tscn")
+
+func close_game():
+	get_tree().quit()
