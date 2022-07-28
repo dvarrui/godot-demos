7af03fa4156a23a40cfa3fcf29da379d869b51de
David Vargas <dvarrui@protonmail.com>
2022-07-28 11:13:00 +0100

    [U] .

--- projects/divarutastic/actor/door/door.gd
+++ projects/divarutastic/actor/door/door.gd
@@ -2,16 +2,24 @@ extends Node2D
 
 export var keys = 1
 
+func _ready():
+	$anim/keys.text = str(keys)
+	
 func open():
-	MyConfig.keys - keys 
+	if MyConfig.keys < keys:
+		return false
+	MyConfig.keys = MyConfig.keys - keys 
 	keys = 0
+	var level = self.get_parent().get_parent()
+	level.update_keys(0)
+
 	$anim.play("open")
 	$anim.play("opened")
-	print($body/shape.disabled)
 	$body/shape.set_disabled(true)
-	print($body/shape.disabled)
+	$body/shape.disabled = true
+	$body.collision_layer = 16
+	#$body/shape.position(self.position.x * -1, self.position.y * -1) # FIXME
 
 func _on_detect_body_entered(body):
 	if body.is_in_group("player"):
-		if MyConfig.keys >= keys:
 			self.open()
