39c54915b4005e46996267ca51d4beed39f76e4a
David Vargas <dvarrui@protonmail.com>
2022-07-30 21:33:11 +0100

    [U] .

--- projects/divarutastic/actor/enemies/enemy1.gd
+++ projects/divarutastic/actor/enemies/enemy1.gd
@@ -10,3 +10,7 @@ func _on_detect_body_entered(body):
 func _on_detect_body_exited(body):
 	if body.is_in_group("player"):
 		$anim.play("sleep")
+
+func _on_enemy1_body_entered(body):
+	if body.is_in_group("player"):
+		body.game_over()
