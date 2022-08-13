06e04d54c24e90118836a5ee031b79b858f53e78
David Vargas <dvarrui@protonmail.com>
2022-08-13 13:30:03 +0100

    [U] .

--- projects/gsh/panel.gd
+++ projects/gsh/panel.gd
@@ -8,6 +8,9 @@ func _on_button_pressed():
 	# Executing command
 	print("[INFO] Executing: ", command)
 	var output = []
+	# execute ( String path, PoolStringArray arguments, 
+	#           bool blocking=true, Array output=[ ], 
+	#           bool read_stderr=false, bool open_console=false )
 	var exitcode = OS.execute(command, [], true, output)
 
 	# Displaying command output	
